2a1713240487ae8b281ff9743029f2dd313d5b2b
CodeForgeX Studio <accounts@codeforgex.studio>
2026-02-06 19:51:37 +0100

    Update auth.tsx

--- app/auth.tsx
+++ app/auth.tsx
@@ -407,7 +407,6 @@ export default function AuthScreen() {
                     showsHorizontalScrollIndicator={false}
                     incognito={false}
                     cacheEnabled={true}
-                    userAgent={`Mozilla/5.0 (iPhone; CPU iPhone OS 18_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1`}
                   />
                 </View>
               </View>
